6bdc1a337fe826f527862be60f2d8f5c1f2e6f47
David Deprost <dadeprost@gmail.com>
2025-11-15 22:00:37 +0100

    Implement game detail tags

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -1,7 +1,11 @@
 /** Generate HTML for the `game` json data and inject it in the `container` HTMLElement:
     <li class='game-card'>
       <div>
-        <h2></h2>
+        <div>
+          <h2></h2>
+          <ul class='genres-list tags></ul>
+          <ul class='platforms-list tags></ul>
+        </div>
         <a href=""></a>
         <img>
       </div>
@@ -14,35 +18,61 @@ export function injectGameHtml(game, collection, container) {
     return; // Skip game and continue the loop
   }
 
-  const div = document.createElement('div');
+  const div1 = document.createElement('div');
+  const div2 = document.createElement('div');
 
-  let name = game.name;
-  const img = document.createElement('img');
-  img.src = game.cover.url.replace('t_thumb', 't_720p');
-  img.alt = name;
-
+  let gameName = game.name;
   const h2 = document.createElement('h2');
   // Remove part of the name after a hyphen if it has one:
   // (Some names are too long because they include useless data, like:
   // The Legend of Zelda: Tears of the Kingdom - Nintendo Switch 2 Edition)
-  if (name.includes('-')) name = name.split('-')[0].trim();
-  h2.textContent = name;
+  if (gameName.includes('-')) gameName = gameName.split('-')[0].trim();
+  h2.textContent = gameName;
+  div2.appendChild(h2);
 
+  // Extra elements for the game detail page:
+  appendTagList(div2, game.genres, "Genres");
+  appendTagList(div2, game.platforms, "Platforms");
+
+  div1.appendChild(div2);
+
+  // Default elements for both sidescroller and game detail page:
   const a = document.createElement('a');
   let path = window.location.pathname;
   path = path.endsWith('/games/') ? path : path + 'games/';
   a.href = `${path}?collection=${collection}&id=${game.id}`;
+  div1.appendChild(a);
 
-  // Append img, h2 and a elements to the div:
-  div.appendChild(h2);
-  div.appendChild(a);
-  div.appendChild(img);
+  const img = document.createElement('img');
+  img.src = game.cover.url.replace('t_thumb', 't_720p');
+  img.alt = gameName;
+  div1.appendChild(img);
 
   // Append the div to the 'game-card' list item:
   const li = document.createElement('li');
   li.classList.add('game-card');
-  li.appendChild(div);
+  li.appendChild(div1);
 
   container.appendChild(li);
 }
 
+function appendTagList(parent, tags, name) {
+  if (!tags) return;
+
+  const tagsTitle = document.createElement('span');
+  tagsTitle.textContent = `${name}: `;
+  const tagsUl = document.createElement('ul');
+  tagsUl.classList.add(`${name}-list`, 'tags');
+  tags.forEach(tag => {
+    const li = document.createElement('li');
+    li.textContent = tag.name;
+    tagsUl.appendChild(li);
+  });
+
+  const tagsDiv = document.createElement('div');
+  tagsDiv.classList.add('tags-row');
+  tagsDiv.appendChild(tagsTitle);
+  tagsDiv.appendChild(tagsUl);
+
+  parent.appendChild(tagsDiv);
+}
